d8d4f68077fa07441d5f9a759b9b964b9f71c97a
sophi1127 <sophi1127@gmail.com>
2022-05-31 02:34:09 +0000

    Modify json files

--- data/data-preprocessing/splitted_dataset.py
+++ data/data-preprocessing/splitted_dataset.py
@@ -41,13 +41,13 @@ preprocessed_data['itemid'] = [int(itemid[idx]) for idx in preprocessed_data['im
 # In[5]:
 
 
-MIN_INTERACTION = 30
+MIN_transaction = 30
 
 agg_on_user = preprocessed_data.groupby("userid").agg({
     "itemid": "nunique"
 })
 
-active_user = agg_on_user[agg_on_user.itemid >= MIN_INTERACTION].index.values
+active_user = agg_on_user[agg_on_user.itemid >= MIN_transaction].index.values
 active_df = preprocessed_data[preprocessed_data.userid.isin(active_user)]
 
 # userid itemid 기준으로 unique한 item만 남김
@@ -85,15 +85,15 @@ test = active_df[active_df['label'] == 'test']
 
 train = active_df[active_df['label'] == 'train']
 train.reset_index(drop=True, inplace=True)
-train.to_csv(f"./train{RECALL_K}_min{MIN_INTERACTION}.csv", index=False)
+train.to_csv(f"./train{RECALL_K}_min{MIN_transaction}.csv", index=False)
 
 test = active_df[active_df['label'] == 'test']
 test.reset_index(drop=True, inplace=True)
-test.to_csv(f"./test{RECALL_K}_min{MIN_INTERACTION}.csv", index=False)
+test.to_csv(f"./test{RECALL_K}_min{MIN_transaction}.csv", index=False)
 assert active_df.purchasing_user_profile_id.nunique()*RECALL_K == test.shape[0]
 
 test_answer = test.sort_values(['userid', 'itemid'])[['userid', 'itemid']]
-test_answer.to_csv(f"./test{RECALL_K}_answer_min{MIN_INTERACTION}.csv", index=False)
+test_answer.to_csv(f"./test{RECALL_K}_answer_min{MIN_transaction}.csv", index=False)
 
 assert len(train.userid.unique()) == len(test.userid.unique())
 print('전체 데이터의 item 개수: ', len(set(active_df.itemid)), '개')
@@ -103,16 +103,17 @@ print('train에 존재하지 않는 test의 item 개수: ', len(set(train.itemid
 # In[7]:
 
 
-def save_interaction_ids(df, id_name):
-    interaction_id = pd.DataFrame(
+def save_transaction_ids(df, id_name):
+    transaction_id = pd.DataFrame(
         {id_name: sorted(df[id_name].unique())}
     )
-    interaction_id_dic = interaction_id.to_json(orient="records")
-    save_json(f"interaction_{id_name}", interaction_id_dic)
+    transaction_id = transaction_id.rename_axis('index').reset_index()
+    transaction_id_dic = transaction_id.to_json(orient="records")
+    save_json(f"transaction_{id_name}", transaction_id_dic)
 
 def save_json(fname, dic_data):
 
-    with open(f"./{fname}_min{MIN_INTERACTION}.json", "w") as outfile:
+    with open(f"./{fname}_min{MIN_transaction}.json", "w") as outfile:
         parsed = json.loads(dic_data)
         json.dump(parsed, outfile, indent=4)
 
@@ -122,12 +123,11 @@ def save_json(fname, dic_data):
 
 itemid_imageurl = active_df[['itemid', 'nifty_obj_img_url']]
 itemid_imageurl = itemid_imageurl.drop_duplicates(subset=["itemid"])
-itemid_imageurl.set_index('itemid', inplace=True)
 itemid_imageurl = itemid_imageurl.to_json(orient="records")
 
-save_json("interaction_itemid_imageurl", itemid_imageurl)
-save_interaction_ids(active_df, "userid")
-save_interaction_ids(active_df, "itemid")
+save_json("transaction_itemid_imageurl", itemid_imageurl)
+save_transaction_ids(active_df, "userid")
+save_transaction_ids(active_df, "itemid")
 
 
 # In[ ]:
