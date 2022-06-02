f647232013a5dcf3bc18592d96930b9e0569fbf7
Jaejeong <48522311+Jaejeong98@users.noreply.github.com>
2022-06-02 16:36:30 +0900

    Add_login

---
+++ front-end/chage_image.js
@@ -0,0 +1,35 @@
+
+// const image1=document.getElementById('image1');
+// var imageUrl = localStorage.getItem("rec_key");
+// var arr=JSON.parse(imageUrl);
+// image1.style.backgroundImage= "url('" + arr[0] + "')"
+
+var imageUrl = localStorage.getItem("rec_key");
+var image_link=localStorage.getItem("rec_value");
+var arr=JSON.parse(imageUrl);
+var arr_link=JSON.parse(image_link);
+console.log(arr_link[0].url[0]);
+
+for(i=1; i<=10; i++){
+    const image1=document.getElementById('image'+i);
+    const video1=document.getElementById('video'+i);
+    if(arr_link[i-1].extension=='jpg'||arr_link[i-1].extension=='png'||arr_link[i-1].extension=='gif'){
+        video1.style.display='none';
+        image1.style.display='block';
+        image1.style.backgroundImage= "url('" + arr[i-1] + "')";
+    }else{
+        image1.style.display='none';
+        video1.style.height='300px'
+        video1.style.width='240px'
+        video1.style.display='block';
+        video1.setAttribute('src',arr[i-1]);
+    }
+    document.getElementById('image'+i+'_link').setAttribute('href',arr_link[i-1].url[0]);
+    document.getElementById("likes"+i).innerHTML="❤ "+arr_link[i-1].likes;
+}
+
+// for(i=1; i<=10; i++){
+//     const image1=document.getElementById('image'+i);
+//     image1.style.backgroundImage= "url('" + arr[i-1] + "')";
+//     document.getElementById('image'+i+'_link').setAttribute('href',arr_link[i-1].url[0]);
+// }
--- front-end/popup.js
+++ front-end/popup.js
@@ -1,3 +1,4 @@
+// localStorage.clear();
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
@@ -15,10 +16,86 @@ function renderURL(statusText) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // when click, get current page link
   var link = document.getElementById('getUrl');
   getCurrentTabUrl(function(url) {
     renderURL(url);
     console.log(url);
   });
 });
+
+// window.onload=function(event){ //페이지 추가 전
+//   document.getElementById('ids').textContent = localStorage.getItem("ID");
+//   if(localStorage.length==0){
+//     function loadPopup(event){
+//       if(document.getElementById("popup-overlay").classList.contains("popup-hide")){
+//         document.getElementById("popup-overlay").classList.remove("popup-hide");
+//        }else{
+//         document.getElementById("popup-overlay").classList.add("popup-show");
+//       }
+//     }
+//     setTimeout(loadPopup, 3000);
+//   }else{
+//     id=localStorage.getItem("ID");
+//     document.getElementById('title').textContent='환영합니다\n'+id+'님!';
+//     // var url="http://115.85.181.5:30001/"+id;
+//     var url='./hugom.json'
+//     fetch(url)
+//         .then((response) => {
+//         return response.json();
+//         })
+//         .then((json) => {
+//             console.log(json);
+//             console.log(json["recommendation"]);
+//             // keys=Object.keys(json.recommendation);
+//             console.log(Object.keys(json.recommendation));
+//             localStorage.setItem("rec_key",JSON.stringify(Object.keys(json.recommendation)));
+//             // values=Object.values(json.recommendation);
+//             console.log(Object.values(json.recommendation));
+//             localStorage.setItem("rec_value",JSON.stringify(Object.values(json.recommendation)));
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+    
+//   }
+// };
+window.onload=function(event){ //페이지 추가 전 json 파일 수정
+  document.getElementById('ids').textContent = localStorage.getItem("ID");
+  if(localStorage.length==0){
+    function loadPopup(event){
+      if(document.getElementById("popup-overlay").classList.contains("popup-hide")){
+        document.getElementById("popup-overlay").classList.remove("popup-hide");
+       }else{
+        document.getElementById("popup-overlay").classList.add("popup-show");
+      }
+    }
+    setTimeout(loadPopup, 3000);
+  }else{
+    id=localStorage.getItem("ID");
+    document.getElementById('title').textContent='환영합니다\n'+id+'님!';
+    // var url="http://115.85.181.5:30001/"+id;
+    // var url='./hugom.json'
+    // var url='./output_hugom.json'
+    var url='./output_artsdesign.json'
+    fetch(url)
+        .then((response) => {
+        return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            console.log(json["recommendation"]);
+            // console.log(Object.keys(json.recommendation));
+            // console.log(Object.values(json.recommendation));
+            keys=Object.keys(json.recommendation);
+            console.log(Object.keys(json.recommendation));
+            localStorage.setItem("rec_key",JSON.stringify(Object.keys(json.recommendation)));
+            values=Object.values(json.recommendation);
+            console.log(Object.values(json.recommendation));
+            localStorage.setItem("rec_value",JSON.stringify(Object.values(json.recommendation)));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    
+  }
+};
